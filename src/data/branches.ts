8888a8cf311598a6d5f48845d6916dd6edb6faa5
infodelta18-creator <infodelta18@gmail.com>
2026-03-10 20:27:37 +0500

    Update branches.ts

--- src/data/branches.ts
+++ src/data/branches.ts
@@ -7,58 +7,13 @@ export type Branch = {
   email?: string;
 };
 
-export const branches: Branch[] = [
+export const branches: Branch[] = 
   {
-    id: "tashkent",
-    city: "Toshkent",
-    address: "Chilonzor tumani, Lutfiy ko‘chasi 47",
-    phones: ["+998 90 002 77 66", "+998 95 989 16 09"],
-    email: "tashkent@daf-fergana.uz",
-  },
-  {
-    id: "bukhara",
-    city: "Buxoro",
-    address: "“Dunyo” binosi",
-    phones: ["+998 90 412 97 95"],
-  },
-  {
-    id: "fergana",
-    city: "Farg‘ona",
-    address: "Turkiston ko‘chasi 22",
-    phones: ["+99 890 535 10 99"],
-    email: "fergana@daf-fergana.uz",
-  },
-  {
-    id: "fergana-texnopark",
-    city: "Farg‘ona",
-    name: "DaF Texnopark",
-    address: "Texnopark, Farg‘ona",
-    phones: ["+99 890 535 10 99"],
-  },
-  {
-    id: "namangan",
-    city: "Namangan",
-    address: "A. Xo‘jayev ko‘chasi 38",
-    phones: ["+998 97 258 12 26"],
-  },
-  {
-    id: "navoiy",
-    city: "Navoiy",
-    address: "Gulbog‘ ko‘chasi 12",
-    phones: ["+998 90 665 25 25"],
-  },
-  {
-    id: "samarkand",
-    city: "Samarqand",
-    address: "Ibn Xoldun ko‘chasi 10B, klinikada tibbiy ekspert",
-    phones: ["+998 90 159 98 88"],
-    email: "samarkand@daf-fergana.uz",
-  },
-  {
-    id: "khorezm",
-    city: "Xorazm",
-    address: "Urganch shahri",
-    phones: ["+998 91 862 36 93"],
+    id: "margilan",
+    city: "Marg'ilon",
+    address: "Marg'ilon, Oygul 3-qavati",
+    phones: ["+99 890 776 15 16"],
+    email: "leadermargilon@gmail.com",
   },
 ];
 
